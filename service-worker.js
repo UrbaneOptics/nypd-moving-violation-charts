7de25d65490aefd27e8c574132f7f470d2b5dfbd
joerodrig <joe.rodriguez@intersection.com>
2019-11-12 16:42:01 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nypd-moving-violation-charts/precache-manifest.70c82daa4195cb3f17af96264fb7547c.js"
+  "/nypd-moving-violation-charts/index.html/precache-manifest.19d96fdd6595ac25309ca4dff3aac7dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nypd-moving-violation-charts/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nypd-moving-violation-charts/index.html/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
