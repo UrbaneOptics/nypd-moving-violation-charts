18dd953f2b21766e5e9b21f2197d701af234f03a
joerodrig <joe.rodriguez@intersection.com>
2019-11-16 20:30:04 -0500

    Add ability to add multiple precincts to chart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MVChart from './MVChart.js';
 
@@ -7,15 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h2>NYPD Moving Violations <small style={{fontSize: '8px' }}>v0.1</small></h2>
-        <p>
-          Select a Precinct or Bureau to get started
-        </p>
+        <h2>NYPD Moving Violations <small style={{fontSize: '8px' }}>v0.2</small></h2>
+        <p>Data retrieved from <a href="https://github.com/UrbaneOptics/nyc-moving-violation-data">github.com/UrbaneOptics/nyc-moving-violation-data</a></p>
+      </header>   
+      <div className="App-body">
         <MVChart />
-        <small>Comments/Suggestions? email joe@urbaneoptics.com</small>
-      <small>Data retrieved from <a href="https://github.com/UrbaneOptics/nyc-moving-violation-data">github.com/UrbaneOptics/nyc-moving-violation-data</a></small>
-      </header>
-      
+      </div>
+      <div className="App-footer">
+      </div>
     </div>
   );
 }
