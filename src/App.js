0a3ed04c5e007a25dc12f2009bf1ee4f68b6676f
joerodrig <joe.rodriguez@intersection.com>
2020-01-10 23:20:38 -0500

    Use rem

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>NYPD Moving Violations <small style={{fontSize: '8px' }}>v0.3</small></h2>
+        <h2>NYPD Moving Violations <small style={{fontSize: '0.5rem' }}>v0.3</small></h2>
         <p>Data retrieved from <a href="https://github.com/UrbaneOptics/nyc-moving-violation-data">github.com/UrbaneOptics/nyc-moving-violation-data</a></p>
       </header>   
       <div className="App-body">
